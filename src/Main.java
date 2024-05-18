688e29ab2f038ce49abedc92e8a96fef60ff39bb
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-05-18 14:56:48 +0400

    Выполненное домашнее задание3

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,18 @@
 public class Main {
     public static void main(String[] args) {
 
-            System.out.println("\nЗадача 1");
-            String firstName = "Ivan ";
-            String middleName = "Ivanovich";
-            String lastName = "Ivanov ";
-            String fullName = "Иванов Семён Семёнович";
-            System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("\nЗадача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
-            System.out.println("\nЗадача 2");
-            System.out.println("Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("\nЗадача 2");
+        System.out.println("Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
-
-            System.out.println("\nЗадача 3");
-            System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace("ё", "е"));
-
+        System.out.println("\nЗадача 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace("ё", "е"));
     }
-    }
+}
