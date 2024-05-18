801f246e8018b6cd56f9ae07800b8342affa2b77
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-05-18 13:00:39 +0400

    Выполненное домашнее задание2

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        {
+
             System.out.println("\nЗадача 1");
             String firstName = "Ivan ";
-            String middleName = "Ivanovich ";
+            String middleName = "Ivanovich";
             String lastName = "Ivanov ";
-            String fullName = lastName + firstName + middleName;
+            String fullName = "Иванов Семён Семёнович";
             System.out.println("Ф. И. О. сотрудника — " + fullName);
 
             System.out.println("\nЗадача 2");
             System.out.println("Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
-        }
-        {
+
+
             System.out.println("\nЗадача 3");
-            String fullName = "Иванов Семён Семёнович";
             System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace("ё", "е"));
-        }
+
     }
     }
